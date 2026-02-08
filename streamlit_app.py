8fd0d1a7a1d535dc5f5258809d72af662e294bd4
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2026-02-08 16:50:09 +0530

    Display smoothie data in a dataframe instead of text
    Replace text output with a dataframe display for smoothie data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -43,6 +43,6 @@ if (ingredients_list and name_on_order):
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

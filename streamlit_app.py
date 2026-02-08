27c1eaf2a51fd059c100cce5007a5ac0dc32b6eb
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2026-02-08 17:24:34 +0530

    Update dataframe display to use pandas conversion
    Comment out the original dataframe display and add conversion to pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,13 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the snowpark dataframe to pandas dataframe so we can use loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
-
-
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients:",

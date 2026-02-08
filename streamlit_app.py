c312fbe31ca761b864017306a64c7addb5e753b8
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2026-02-08 12:28:37 +0530

    Fix typo in connection method for Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie!")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.connecion("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))

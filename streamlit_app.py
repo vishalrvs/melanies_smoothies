23f51373afc012edd5746cbc4ea602cb37d92843
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2026-02-08 16:58:13 +0530

    Fetch nutrition information for each ingredient

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ if ingredients_list:
 
     for x in ingredients_list:
         ingredients_string += x + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(x + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+x)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
